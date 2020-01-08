ca368fe29f81f5559f117775fcf0f7ec417f7656
Milkey Tan <819774989@qq.com>
2020-01-08 20:49:52 +0800

    sth

---
+++ GetPlaylist/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GetPlaylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint pid = 60079540;
+            List<string> list = new List<string>();
+            var jsonStr = new WebClient {Encoding = Encoding.UTF8}.DownloadString("https://v1.hitokoto.cn/nm/playlist/" + pid);
+            var json = JsonConvert.DeserializeObject<dynamic>(jsonStr);
+            foreach (var itemTrack in json.playlist.tracks)
+            {
+                list.Add(itemTrack.id.ToString());
+                Console.WriteLine(itemTrack.id.ToString());
+            }
+
+            File.WriteAllLines(pid + "playlist.txt", list);
+            Console.ReadKey();
+        }
+    }
+}
