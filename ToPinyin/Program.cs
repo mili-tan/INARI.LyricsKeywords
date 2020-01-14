92c3e3a2a2ac8325eaf9ad94aee8404acbe2a292
Milkey Tan <819774989@qq.com>
2020-01-14 21:06:29 +0800

    fix dict

--- ToPinyin/Program.cs
+++ ToPinyin/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Xml;
 
 namespace ToPinyin
@@ -29,8 +30,6 @@ namespace ToPinyin
                     }
                 }
             }
-
-
         }
     }
 }
